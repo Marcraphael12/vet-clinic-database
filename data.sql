e4bb1d3374ef91b99b8c39b01eeeda17ba967892
Marc raphael <marcraphael979@gmail.com>
2023-03-30 13:51:14 +0100

    Create foreign keys to animals table

--- data.sql
+++ data.sql
@@ -14,3 +14,49 @@ VALUES
 	('Boarmon', '2005-06-07', 7, true, 20.4),
 	('Blossom', '1998-10-13', 3, true, 17.0),
 	('Ditto', '2022-05-14', 4, true, 22.0);
+
+-- update the owners table
+insert into owners (full_name, age) values
+	('Sam Smith', 34),
+	('Jennifer Orwell', 19),
+	('Bob', 45),
+	('Melody Pond', 77),
+	('Dean Winchester', 14),
+	('Jodie Whittaker', 38);
+
+-- update the species table
+insert into species (name) values
+	('Pokemon'),
+	('Digimon');
+
+-- Modify the inserted animals so it includes the species_id value
+-- If the name ends in "mon" it will be Digimon
+update animals 
+set species_id = (select id from species where name = 'Digimon')
+where name like '%mon';
+
+-- All other animals are Pokemon
+update animals
+set species_id = (select id from species where name = 'Pokemon')
+where species_id is NULL;
+
+-- Modify the inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+update animals
+set owner_id = (select owners_id from owners where full_name = 'Sam Smith') where name = 'Agumon';
+
+-- Jennifer Orwell owns Gabumon and Pikachu.
+update animals
+set owner_id = (select owners_id from owners where full_name = 'Jennifer Orwell') where name = 'Gabumon' or name = 'Pikachu';
+
+-- Bob owns Devimon and Plantmon.
+update animals
+set owner_id = (select owners_id from owners where full_name = 'Bob') where name = 'Devimon' or name = 'Plantmon';
+
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+update animals
+set owner_id = (select owners_id from owners where full_name = 'Melody Pond') where name = 'Charmander' or name = 'Squirtle' or name = 'Blossom';
+
+-- Dean Winchester owns Angemon and Boarmon.
+update animals
+set owner_id = (select owners_id from owners where full_name = 'Dean Winchester') where name = 'Angemon' or name = 'Boarmon';
